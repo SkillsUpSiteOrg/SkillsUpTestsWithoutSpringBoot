9580f2d2fb600b5d49156213e607d1dbfa9d60d7
Dudarev Daniel <dudarev.daniel@gmail.com>
2014-12-25 19:47:00 +0200

    Main and Tomcat are running well. Tests not running.

--- src/main/java/ua/dp/skillsup/tests/ApplicationMain.java
+++ src/main/java/ua/dp/skillsup/tests/ApplicationMain.java
@@ -1,11 +1,8 @@
 package ua.dp.skillsup.tests;
 
 import org.joda.time.DateTime;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 import ua.dp.skillsup.tests.dao.ApplicationDAO;
 import ua.dp.skillsup.tests.dao.entity.TestDescription;
 
--- src/test/java/dao/ApplicationDaoTests.java
+++ src/test/java/dao/ApplicationDaoTests.java
@@ -1,5 +1,6 @@
 package dao;
 
+import org.apache.log4j.Logger;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.IDataSet;
@@ -7,9 +8,11 @@ import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.dbunit.operation.DatabaseOperation;
 import org.joda.time.DateTime;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
@@ -18,39 +21,38 @@ import org.springframework.test.AbstractTransactionalDataSourceSpringContextTest
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ua.dp.skillsup.tests.dao.ApplicationDAO;
-import ua.dp.skillsup.tests.dao.ApplicationDAOImpl;
 import ua.dp.skillsup.tests.dao.entity.TestDescription;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.util.List;
-import java.util.logging.Logger;
 
 /**
  * Created by Daniel on 23.12.2014.
  */
-//@Ignore
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations ="classpath:applicationContext.xml")
+@Ignore
+@RunWith(SpringJUnit4ClassRunner.class)       //BlockJUnit4ClassRunner
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
 public class ApplicationDaoTests extends AbstractTransactionalDataSourceSpringContextTests {
 
-    @Autowired
-    @Qualifier("applicationDao")
+    /*@Autowired
+    @Qualifier("applicationDao")*/
     private ApplicationDAO dao;
 
-    //private static final Logger logger = Logger.getLogger(ApplicationDaoTests.class);
+    private static final Logger logger = Logger.getLogger(ApplicationDaoTests.class);
 
     public ApplicationDaoTests() {
         super();
         logger.info("setting up test");
-        /*ApplicationContext ctx = super.getApplicationContext();
-        dao = (ApplicationDAO) ctx.getBean("applicationDao");*/
+        ApplicationContext ctx = super.getApplicationContext();
+        dao = (ApplicationDAO) ctx.getBean("applicationDao");
+        //System.out.println("All TD from DAO:" + dao.getAllTestDescriptions());
         assertNotNull(dao);
     }
 
     @Override
     protected String[] getConfigLocations() {
-        return new String[] {"classpath:applicationContext.xml"};
+        return new String[] {"applicationContext.xml"};
     }
 
     @Override
@@ -60,7 +62,7 @@ public class ApplicationDaoTests extends AbstractTransactionalDataSourceSpringCo
         IDatabaseConnection dbUnitCon = new DatabaseConnection(con);
         IDataSet dataSet = new FlatXmlDataSetBuilder().build(
                 Thread.currentThread().getContextClassLoader()
-                        .getResourceAsStream("testDescription-data.xml"));
+                        .getResourceAsStream("classpath:testDescription-data.xml"));
         try {
             DatabaseOperation.REFRESH.execute(dbUnitCon, dataSet);
         } finally {
@@ -68,23 +70,38 @@ public class ApplicationDaoTests extends AbstractTransactionalDataSourceSpringCo
         }
     }
 
-    @Test
-    public void testGetAllTestDescriptions() throws Exception {
-        List<TestDescription> testDescriptions = dao.getAllTestDescriptions();
-        assertEquals("did not get expected number of entities ", 2, testDescriptions.size());
+    @Before
+    public void setUpTests(){
+        TestDescription testDescription1 = new TestDescription();
+        testDescription1.setTestName("Java-0");
+        testDescription1.setDateOfCreation(new DateTime(2014, 11, 15, 0, 0));
+        testDescription1.setMaxTimeToPassInMinutes(90);
+
+        TestDescription testDescription2 = new TestDescription();
+        testDescription2.setTestName("Java-1");
+        testDescription2.setDateOfCreation(new DateTime(2010, 2, 8, 0, 0));
+        testDescription2.setMaxTimeToPassInMinutes(120);
+
+        dao.addTestDescription(testDescription1);
+        dao.addTestDescription(testDescription2);
     }
 
     @Test
     public void testAddTestDescription() throws Exception {
-        TestDescription testDescription = new TestDescription();
-        testDescription.setTestName("Java-0");
-        testDescription.setDateOfCreation(new DateTime(2014, 12, 15, 0, 0));
-        testDescription.setMaxTimeToPassInMinutes(90);
 
-        dao.addTestDescription(testDescription);
-        TestDescription testDescriptionActual = dao.getTestDescription(3L);
-        String[] ignore = {"id"};
-        assertNotNull("expected save to work", testDescriptionActual);
+        TestDescription testDescription1Actual = dao.getTestDescription(1L);
+        TestDescription testDescription2Actual = dao.getTestDescription(2L);
+
+        //String[] ignore = {"id"};
+        assertNotNull("expected save to work", testDescription1Actual);
+        assertNotNull("expected save to work", testDescription2Actual);
+    }
+
+    @Test
+    public void testGetAllTestDescriptions() throws Exception {
+        List<TestDescription> testDescriptions = dao.getAllTestDescriptions();
+        System.out.println(testDescriptions);
+        assertEquals("did not get expected number of entities ", 2, testDescriptions.size());
     }
 
     @Test
