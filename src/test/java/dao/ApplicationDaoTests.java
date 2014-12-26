c5ebacabe02e0eee736e546af1e3361e90176d92
Dudarev Daniel <dudarev.daniel@gmail.com>
2014-12-26 10:37:50 +0200

    Main, Tomkat and Tests are work fine.

--- src/test/java/dao/ApplicationDaoTests.java
+++ src/test/java/dao/ApplicationDaoTests.java
@@ -4,6 +4,7 @@ import org.apache.log4j.Logger;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.dbunit.operation.DatabaseOperation;
 import org.joda.time.DateTime;
@@ -24,26 +25,22 @@ import ua.dp.skillsup.tests.dao.ApplicationDAO;
 import ua.dp.skillsup.tests.dao.entity.TestDescription;
 
 import javax.sql.DataSource;
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.util.List;
 
 /**
  * Created by Daniel on 23.12.2014.
  */
-@Ignore
-@RunWith(SpringJUnit4ClassRunner.class)       //BlockJUnit4ClassRunner
-@ContextConfiguration(locations = "classpath:applicationContext.xml")
-public class ApplicationDaoTests extends AbstractTransactionalDataSourceSpringContextTests {
+//@Ignore
+//@RunWith(SpringJUnit4ClassRunner.class)       //BlockJUnit4ClassRunner
+//@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class ApplicationDaoTests extends  AbstractTransactionalDataSourceSpringContextTests {
 
-    /*@Autowired
-    @Qualifier("applicationDao")*/
     private ApplicationDAO dao;
-
-    private static final Logger logger = Logger.getLogger(ApplicationDaoTests.class);
 
     public ApplicationDaoTests() {
         super();
-        logger.info("setting up test");
         ApplicationContext ctx = super.getApplicationContext();
         dao = (ApplicationDAO) ctx.getBean("applicationDao");
         //System.out.println("All TD from DAO:" + dao.getAllTestDescriptions());
@@ -62,39 +59,28 @@ public class ApplicationDaoTests extends AbstractTransactionalDataSourceSpringCo
         IDatabaseConnection dbUnitCon = new DatabaseConnection(con);
         IDataSet dataSet = new FlatXmlDataSetBuilder().build(
                 Thread.currentThread().getContextClassLoader()
-                        .getResourceAsStream("classpath:testDescription-data.xml"));
+                        .getResourceAsStream("testDescription-data.xml"));
         try {
             DatabaseOperation.REFRESH.execute(dbUnitCon, dataSet);
+            //System.out.println("!!!!!!!!!!!!!!!" + dao.getAllTestDescriptions());
         } finally {
-            DataSourceUtils.releaseConnection(con, dataSource);
+            //DataSourceUtils.releaseConnection(con, dataSource);
         }
-    }
 
-    @Before
-    public void setUpTests(){
-        TestDescription testDescription1 = new TestDescription();
-        testDescription1.setTestName("Java-0");
-        testDescription1.setDateOfCreation(new DateTime(2014, 11, 15, 0, 0));
-        testDescription1.setMaxTimeToPassInMinutes(90);
-
-        TestDescription testDescription2 = new TestDescription();
-        testDescription2.setTestName("Java-1");
-        testDescription2.setDateOfCreation(new DateTime(2010, 2, 8, 0, 0));
-        testDescription2.setMaxTimeToPassInMinutes(120);
-
-        dao.addTestDescription(testDescription1);
-        dao.addTestDescription(testDescription2);
     }
 
     @Test
     public void testAddTestDescription() throws Exception {
 
-        TestDescription testDescription1Actual = dao.getTestDescription(1L);
-        TestDescription testDescription2Actual = dao.getTestDescription(2L);
+        TestDescription testDescription3 = new TestDescription();
+        testDescription3.setTestName("Java-2");
+        testDescription3.setDateOfCreation(new DateTime(2014, 11, 15, 0, 0));
+        testDescription3.setMaxTimeToPassInMinutes(90);
+        dao.addTestDescription(testDescription3);
 
+        TestDescription testDescription3Actual = dao.getTestDescription(3L);
         //String[] ignore = {"id"};
-        assertNotNull("expected save to work", testDescription1Actual);
-        assertNotNull("expected save to work", testDescription2Actual);
+        assertNotNull("expected save to work", testDescription3Actual);
     }
 
     @Test
